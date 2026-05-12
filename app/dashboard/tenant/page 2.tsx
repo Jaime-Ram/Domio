320dfb252d0ee617cd9dffc5fb96e0b18cf65180
jandomhof <j.h.domhof@outlook.com>
2026-05-12 17:38:27 +0200

    removed random copied files

--- app/dashboard/tenant/page 2.tsx
+++
@@ -1,7 +0,0 @@
-export default function TenantDashboardPage() {
-  return (
-    <div>
-      <h1 className="text-2xl font-semibold text-gray-900 dark:text-gray-100">Dashboard</h1>
-    </div>
-  )
-}
